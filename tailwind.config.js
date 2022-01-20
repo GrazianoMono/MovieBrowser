a10a26e83da68ab0d133d381fae4482e015a5aae
JustSampei <graziano@GrazianoArch>
2022-01-20 16:39:16 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		fontFamily: {
+			roboto: 'Roboto',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { NONAME } = require('dns')
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		compress: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Portfolio',
+			template: './src/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader', 'postcss-loader'],
+				exclude: '/node_modules',
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+				exclude: '/node_modules',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+				exclude: '/node_modules',
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+				exclude: '/node_modules',
+			},
+		],
+	},
+}
